9371b8bb9a701f4357aba5510e017bc4ffa22bd7
Christopher Tserenkov <chris@ChristorsLaptop.lan>
2024-08-28 20:11:23 +0300

    add backend url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -4,25 +4,29 @@
 import axios from 'axios';
 import { ACCESS_TOKEN } from './constants';
 
+const apiUrl = '/choreo-apis/djangoreacttutorial/backend/v1';
+
 const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL
-})
+        ? import.meta.env.VITE_API_URL
+        : apiUrl,
+});
 
 api.interceptors.request.use(
     // Function that checks for an access token
     (config) => {
-        // Try to access token from local storage 
+        // Try to access token from local storage
         const token = localStorage.getItem(ACCESS_TOKEN);
         // Check if the token exists
         if (token) {
             // Add the token
-            config.headers.Authorization = `Bearer ${token}`
-        } 
-        return config
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
     },
     (error) => {
-        return Promise.reject(error)
+        return Promise.reject(error);
     }
-)
+);
 
-export default api
+export default api;
